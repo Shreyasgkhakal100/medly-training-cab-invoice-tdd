bed5de3b5ca383379def30546c1d9689a2a700ff
Shreyas Khakal <shreyasgkhakal100@gmail.com>
2022-05-24 10:46:21 +0530

    Add test cases for multple rides

--- invoice/src/Invoice.kt
+++ invoice/src/Invoice.kt
@@ -1,12 +1,14 @@
 
 class Invoice {
     companion object {
-        val RATE_PER_KM = 10.0
-        val RATE_PER_MIN = 1.0
+        const val RATE_PER_KM = 10.0
+        const val RATE_PER_MIN = 1.0
     }
     fun create(rides: List<Ride>): Double {
-        val ride = rides[0]
-        return ride.dist * RATE_PER_KM + ride.time * RATE_PER_MIN
+        val total = rides.sumOf {
+            it.dist * RATE_PER_KM + it.time * RATE_PER_MIN
+        }
+        return total
     }
 
 }
--- invoice/test/InvoiceTest.kt
+++ invoice/test/InvoiceTest.kt
@@ -43,14 +43,40 @@ class InvoiceTest: StringSpec() {
             calculateFare shouldBeExactly 11.0
         }
 
-//        "should run for two rides in a month" {
-//            val invoice = Invoice()
-//            val ride1 = Ride(1.0, 1.0)
-//            val ride2 = Ride(1.0, 1.0)
-//            val rides = listOf<Ride>(ride1, ride2)
-//            val calculateFare = invoice.create(rides)
-//            calculateFare shouldBeExactly 22.0
-//        }
+        "should run for two rides in a month" {
+            val invoice = Invoice()
+            val ride1 = Ride(1.0, 1.0)
+            val ride2 = Ride(1.0, 1.0)
+            val rides = listOf<Ride>(ride1, ride2)
+            val calculateFare = invoice.create(rides)
+            calculateFare shouldBeExactly 22.0
+        }
+
+        "should run for two rides having fractional values" {
+            val invoice = Invoice()
+            val ride1 = Ride(1.0, 1.0)
+            val ride2 = Ride(2.5, 1.3)
+            val rides = listOf<Ride>(ride1, ride2)
+            val calculateFare = invoice.create(rides)
+            calculateFare shouldBeExactly 37.3
+        }
+
+        "should run for two rides having fractional values and one having both values equal to zero" {
+            val invoice = Invoice()
+            val ride1 = Ride(1.0, 1.0)
+            val ride2 = Ride(1.5, 1.5)
+            val ride3 = Ride(0.0, 0.0)
+            val rides = listOf<Ride>(ride1, ride2, ride3)
+            val calculateFare = invoice.create(rides)
+            calculateFare shouldBeExactly 27.5
+        }
+
+        "should return 0.0 if no rides are given" {
+            val invoice = Invoice()
+            val rides = listOf<Ride>()
+            val calculateFare = invoice.create(rides)
+            calculateFare shouldBeExactly 0.0
+        }
 
     }
 }
